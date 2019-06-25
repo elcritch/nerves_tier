ee9ca0e61c9458e4ee06d883f7b245b297095371
Jaremy Creechley <creechley@gmail.com>
2019-06-25 17:37:43 -0600

    fixing issues with init

--- lib/nerves_tier.ex
+++ lib/nerves_tier.ex
@@ -2,24 +2,34 @@ defmodule NervesTier do
   use GenServer
   require Logger
 
+  @zt_home "/root/.zt/"
+  @zt_port 9993
   defp token!() do
     File.read!(Path.join(@zt_home, "/authtoken.secret"))
   end
 
   def info() do
-    HTTPoison.get!("127.0.0.1:#{@zt_port}/status?auth=#{token!()}") |> Poison.decode!()
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/status?auth=#{token!()}").body |> Poison.decode!()
   end
 
   def networks() do
-    HTTPoison.get!("127.0.0.1:#{@zt_port}/networks?auth=#{token!()}") |> Poison.decode!()
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/networks?auth=#{token!()}").body |> Poison.decode!()
   end
 
   def network(id) do
-    HTTPoison.get!("127.0.0.1:#{@zt_port}/network/#{id}?auth=#{token!()}") |> Poison.decode!()
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/network/#{id}?auth=#{token!()}").body |> Poison.decode!()
+  end
+
+  def network_join(id, params \\ %{}) do
+    HTTPoison.post!("127.0.0.1:#{@zt_port}/network/#{id}?auth=#{token!()}", params |> Poison.encode!()).body |> Poison.decode!()
+  end
+
+  def network_leave(id) do
+    HTTPoison.delete!("127.0.0.1:#{@zt_port}/network/#{id}?auth=#{token!()}").body |> Poison.decode!()
   end
 
   def peers() do
-    HTTPoison.get!("127.0.0.1:#{@zt_port}/networks?auth=#{token!()}") |> Poison.decode!()
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/networks?auth=#{token!()}").body |> Poison.decode!()
   end
 
 end
@@ -30,7 +40,6 @@ defmodule NervesTier.PortServer do
 
   # Callbacks
   @zt_home "/root/.zt/"
-  @zt_port 9993
 
   def start_link(args \\ [], opts \\ []) do
     GenServer.start_link(__MODULE__, args, opts ++ [name: __MODULE__])
@@ -45,6 +54,8 @@ defmodule NervesTier.PortServer do
 
   @impl true
   def handle_cast(:start, state) do
+
+     {_, 0} = System.cmd("modprobe", ["tun"])
 
     zt_bin =  "#{:code.priv_dir(:nerves_tier)}/usr/sbin/zerotier-one"
     Logger.info("Starting ZerotierOne")
