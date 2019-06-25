b869935d9ca15a848ff9b3ba7e224392e1a66d7d
Jaremy Creechley <creechley@gmail.com>
2019-06-25 13:58:36 -0600

    initial makefile

---
+++ .elixir_ls/build/custom_rpi3_test/lib/nerves_tier/ebin/nerves_tier.app
@@ -0,0 +1,6 @@
+{application,nerves_tier,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"nerves_tier"},
+              {modules,['Elixir.NervesTier']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule NervesTier.MixProject do
     [
       app: :nerves_tier,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
+      compilers: [:elixir_make] ++ Mix.compilers,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +22,7 @@ defmodule NervesTier.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:elixir_make, "~> 0.4", runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
