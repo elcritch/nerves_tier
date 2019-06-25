090136fa2703e47cdb071378b36fbb16953dc972
Jaremy Creechley <creechley@gmail.com>
2019-06-25 16:36:23 -0600

    separate api from port server

--- lib/nerves_tier.ex
+++ lib/nerves_tier.ex
@@ -2,12 +2,38 @@ defmodule NervesTier do
   use GenServer
   require Logger
 
+  defp token!() do
+    File.read!(Path.join(@zt_home, "/authtoken.secret"))
+  end
+
+  def info() do
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/status?auth=#{token!()}") |> Poison.decode!()
+  end
+
+  def networks() do
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/networks?auth=#{token!()}") |> Poison.decode!()
+  end
+
+  def network(id) do
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/network/#{id}?auth=#{token!()}") |> Poison.decode!()
+  end
+
+  def peers() do
+    HTTPoison.get!("127.0.0.1:#{@zt_port}/networks?auth=#{token!()}") |> Poison.decode!()
+  end
+
+end
+
+defmodule NervesTier.PortServer do
+  use GenServer
+  require Logger
+
   # Callbacks
   @zt_home "/root/.zt/"
   @zt_port 9993
 
-  def start_link(default) when is_list(default) do
-    GenServer.start_link(__MODULE__, default)
+  def start_link(args \\ [], opts \\ []) do
+    GenServer.start_link(__MODULE__, args, opts ++ [name: __MODULE__])
   end
 
   @impl true
@@ -27,13 +53,10 @@ defmodule NervesTier do
     {:noreply, %{ state | port: port} }
   end
 
-  defp token!() do
-    File.read!(Path.join(@zt_home, "/authtoken.secret"))
+  def handle_call(:status, state) do
+    {:replay, Port.info(state.port), state}
   end
 
-  def info() do
-    HTTPoison.get!("127.0.0.1:#{@zt_port}/status?auth=#{token!()}")
-  end
 end
 
 defmodule NervesTier.Application do
@@ -45,7 +68,7 @@ defmodule NervesTier.Application do
 
   def start(_type, _args) do
     children = [
-      NervesTier,
+      NervesTier.PortServer,
     ]
     Supervisor.start_link(children, strategy: :one_for_one)
   end
