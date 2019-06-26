dcaf2ba2790c6a6c00426cc0318ea8f6780b0567
Jaremy Creechley <creechley@gmail.com>
2019-06-25 19:50:52 -0600

    Manually kill zt pid after process exits

--- lib/nerves_tier.ex
+++ lib/nerves_tier.ex
@@ -51,14 +51,22 @@ defmodule NervesTier.PortServer do
   def init(args \\ []) do
 
     GenServer.cast(self(), :start)
+    Process.flag(:trap_exit, true)
     {:ok, %{port: nil}}
   end
 
   @impl true
   def handle_info(:check, state) do
     unless state.port |> Port.info() do
-      raise "zerotier port died"
+      Logger.error "zerotier port died"
+      # raise "zerotier port died"
     end
+    {:noreply, state }
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    Logger.error "zerotier info: #{inspect msg}"
     {:noreply, state }
   end
 
@@ -68,18 +76,36 @@ defmodule NervesTier.PortServer do
 
      {_, 0} = System.cmd("modprobe", ["tun"])
 
-    run_zt =  "#{:code.priv_dir(:nerves_tier)}/run-zt.sh"
+    # run_zt =  "#{:code.priv_dir(:nerves_tier)}/run-zt.sh"
     zt_bin =  "#{:code.priv_dir(:nerves_tier)}/usr/sbin/zerotier-one"
-    port = Port.open({:spawn_executable, run_zt}, [:binary, args: [zt_bin, @zt_home]])
+    port = Port.open({:spawn_executable, zt_bin}, [:binary, args: [@zt_home]])
+    # port = Port.open({:spawn_executable, run_zt}, [:binary, args: [zt_bin, @zt_home]])
     Process.send_after(self(), :check, 1_000, [])
 
     {:noreply, %{ state | port: port} }
   end
 
   def handle_call(:status, state) do
-    {:replay, Port.info(state.port), state}
+    {:reply, Port.info(state.port), state}
   end
 
+  def handle_call(:port, _from, state) do
+    {:reply, state.port, state}
+  end
+
+  def terminate(reason, state) do
+    Logger.error "#{__MODULE__}.terminate/2 called with reason: #{inspect reason}"
+    port_info = Port.info(state.port)
+    Logger.error("#{__MODULE__}.termindate port: #{inspect port_info}")
+
+    case port_info[:os_pid] do
+      ospid when is_number(ospid) ->
+        Logger.info "#{__MODULE__} terminate port, os process: #{inspect ospid}"
+        System.cmd("kill", ["-KILL", "#{ospid}"], [])
+      nil ->
+        nil
+    end
+  end
 end
 
 defmodule NervesTier.Application do
